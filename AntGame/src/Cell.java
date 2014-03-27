77e81014b0813aa4bf211b7aaedfcc09e31647d5
kornie210 <kr210@sussex.ac.uk>
2014-03-27 19:53:20 +0000

    Create Cell.java

---
+++ AntGame/src/Cell.java
@@ -0,0 +1,69 @@
+/**
+ * A class to represent a cell
+ * @author K Ratusznik
+ * @version 27/03/2014
+ */
+public class Cell {
+	public CellType state;
+	private Ant ant;
+	private int foodAmount;
+	private boolean redMarkers;
+	private boolean blackMarkers;
+	
+	/**
+	 * Construct an object
+	 * @param state State of a cell
+	 * @param ant 
+	 * @param foodAmount
+	 * @param redMarkers
+	 * @param blackMarkers
+	 */
+	public Cell(CellType state, Ant ant, int foodAmount, boolean redMarkers, boolean blackMarkers) {
+		this.state = state;
+		this.ant = ant;
+		this.foodAmount = foodAmount;
+		this.redMarkers = redMarkers;
+		this.blackMarkers = blackMarkers;
+	}
+	
+	// Setters and getters
+	public Ant getAnt() {
+		return ant;
+	}
+
+	public void setAnt(Ant ant) {
+		this.ant = ant;
+	}
+
+	public int getFoodAmount() {
+		return foodAmount;
+	}
+
+	public void setFoodAmount(int foodAmount) {
+		this.foodAmount = foodAmount;
+	}
+
+	public boolean isRedMarkers() {
+		return redMarkers;
+	}
+
+	public void setRedMarkers(boolean redMarkers) {
+		this.redMarkers = redMarkers;
+	}
+
+	public boolean isBlackMarkers() {
+		return blackMarkers;
+	}
+
+	public void setBlackMarkers(boolean blackMarkers) {
+		this.blackMarkers = blackMarkers;
+	}
+
+	public CellType getState() {
+		return state;
+	}
+	
+	public enum CellType {
+		CLEARCELL, ROCKYCELL 
+	}
+}
