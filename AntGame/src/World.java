ebd0c5ce3aba71d55dafeb83335f9713fdea2bf5
davorink <davorin.kopic@gmail.com>
2014-03-27 19:12:38 +0000

    Created pos, updated World.

--- AntGame/src/World.java
+++ AntGame/src/World.java
@@ -1,11 +1,106 @@
 /**
- * 
- */
-
-/**
  * @author Davorin Kopic
- *
+ * @version 27/3/2014
  */
 public class World {
-
+	private Cell[][] cells;
+	private Ant[] ants;
+	
+	/**
+	 * Constructor
+	 * @param cells 2D array of Cells to construct the world
+	 * @param ants Array of ants in the World
+	 */
+	public World(Cell[][] cells, Ant[] ants){
+		this.cells = cells;
+		this.ants = ants;
+	}
+	
+	/**
+	 * Checks if the Cell at position p is rocky.
+	 * @param p Position in the World
+	 * @return true if the cell at position p is rocky
+	 */
+	public boolean rocky(pos p){
+		return cells[p.getX()][p.getY()].getState() == ROCKY;
+	}
+	
+	/**
+	 * Checks if the Cell contains an Ant.
+	 * @param p Position in the World
+	 * @return true if there is an ant in the cell at position p
+	 */
+	public boolean some_ant_is_at(pos p){
+		return cells[p.getX()][p.getY()].getAnt() == null;
+	}
+	
+	/**
+	 * Returns the Ant from position p, null if no Ant.
+	 * @param p Position in the World
+	 * @return Ant if there is an ant in the cell at position p, null otherwise
+	 */
+	public Ant ant_at(pos p){
+		if(some_ant_is_at(p)){
+			return cells[p.getX()][p.getY()].getAnt();
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * Record the fact that the given ant is at position p
+	 * @param p Position in the World
+	 * @param a Ant to be set
+	 */
+	public void set_ant_at(pos p, Ant a){
+		cells[p.getX()][p.getY()].setAnt(a);
+	}
+	
+	/**
+	 * Record the fact that no ant is at position p
+	 * @param p Position in the World
+	 */
+	public void clear_ant_at(pos p){
+		cells[p.getX()][p.getY()].setAnt(null);
+	}
+	
+	
+	/**
+	 * Checks if the Ant with id is alive.
+	 * @param id Ant's id number
+	 * @return true if an ant with the given id exists somewhere in the world
+	 */
+	public boolean ant_is_alive(int id){
+		for(Ant a : ants){
+			if(a.getID() == id)return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * XXXXXXX Check if works!!!
+	 * Returns the position of the given Ant. If Ant does not exist, returns null.
+	 * @param id Ant's id number
+	 * @return current position of the ant with the given id, null if not existing
+	 */
+	public pos find_ant(int id){
+		for(int x=0; x!=cells.size(); x++){
+			for(int y=0; y!=cells[x].size(); y++){
+				if(cells[x][y].getAnt() != null){
+					if(cells[x][y].getAnt().getID == id){
+						return new pos(x,y);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Kills ant at position p.
+	 * @param pos Ant's position
+	 */
+	public void kill_ant_at(pos p){
+		clear_ant_at(p);
+	}
 }
---
+++ AntGame/src/pos.java
@@ -0,0 +1,49 @@
+/**
+ * Represents a position in the World.
+ * @author Davorin Kopic
+ * @version 27/3/2014
+ */
+public class pos {
+	private int x;
+	private int y;
+	
+	/**
+	 * Constructor
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
+	public pos(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Returns x coordinate.
+	 */
+	public int getX() {
+		return x;
+	}
+	
+	/**
+	 * Set x coordinate.
+	 * @param x
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	/**
+	 * Returns y coordinate.
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * Set y coordinate.
+	 * @param y
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+}
