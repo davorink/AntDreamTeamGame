9e137bcc7fa0794f105f5e930e78afc3826dc0f7
tippler2000 <kerry_hutchings@outlook.com>
2014-03-31 15:11:45 +0100

    Test

--- AntGame/src/Brain.java
+++ AntGame/src/Brain.java
@@ -3,31 +3,25 @@ import java.util.ArrayList;
 /*
  * A class to represent an ant brain
  * @author K Hutchings
- * @version 26/03/14
+ * @version 31/03/14
  */
 public class Brain {
-	private int id;
 	private ArrayList<String> instructions;
-	private int currentInstruction;
 	
 	/*
 	 * Create the brain object
 	 * @param instructions The instructions for the brain
-	 * @param andID The ID of the ant the brain is for
 	 */
-	public Brain(ArrayList<String> instructions, int id) {
-		this.id = id;
+	public Brain(ArrayList<String> instructions) {
 		this.instructions = instructions;
-		this.currentInstruction = -1;
 	}
 	
 	/*
-	 * Return the next instruction
+	 * Return the requested instruction
 	 * @return The next instruction
 	 */
-	public String getNextInstruction() {
-		currentInstruction ++;
-		return instructions.get(currentInstruction);
+	public String getNextInstruction(int state) {
+		return instructions.get(state);
 	}
 	
 }
