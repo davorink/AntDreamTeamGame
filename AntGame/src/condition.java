89dfc9074d6e15d6a90299e4ad94b539b37552af
davorink <davorin.kopic@gmail.com>
2014-03-31 15:38:27 +0100

    Create condition.java

---
+++ AntGame/src/condition.java
@@ -0,0 +1,17 @@
+/**
+ * condition.java represents the condition for the Ant Game. 
+ * @author Davorin Kopic
+ * @version 31/3/2014
+ */
+public class condition {
+	Friend,             /* cell contains an ant of the same color */
+  Foe,                /* cell contains an ant of the other color */
+  FriendWithFood,     /* cell contains an ant of the same color carrying food */
+  FoeWithFood,        /* cell contains an ant of the other color carrying food */
+  Food,               /* cell contains food (not being carried by an ant) */
+  Rock,               /* cell is rocky */
+  Marker(marker),     /* cell is marked with a marker of this ant's color */
+  FoeMarker,          /* cell is marked with *some* marker of the other color */
+  Home,               /* cell belongs to this ant's anthill */
+  FoeHome             /* cell belongs to the other anthill */
+}
