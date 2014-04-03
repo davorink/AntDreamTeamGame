94eaecae2c3fd49094de3e43ae6f61bb3abbcca7
tippler2000 <kerry_hutchings@hotmail.com>
2014-04-03 18:20:09 +0100

    Update AntFactory.java
    Manual update (sync issue)

--- AntGame/src/AntFactory.java
+++ AntGame/src/AntFactory.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 
-/*
+/**
  * A class to represent an ant factory
  * @author K Hutchings
- * @version 31/03/14
+ * @version 03/04/14
  */
 public class AntFactory {
 	ArrayList<Ant> ants;
 	
-	/*
-	 * Create a team of ants object with -1 as id (set in world)
+	/**
+	 * Create a team of ants objects
 	 * @param amount The amount of ants to create
 	 * @param colour The colour the ant team is
 	 */
-	public AntFactory(int amount, Color color) {
+	public AntFactory(int amount, TeamColour color) {
 		ants = new ArrayList<Ant>();
 		int id = 0;
 		for (int i=0; i<amount; i++) {
@@ -21,7 +21,7 @@ public class AntFactory {
 		}
 	}
 	
-	/*
+	/**
 	 * Return an arrayList of the team of ants
 	 * @return An array list containing the ants in the team
 	 */
